08568fc28bdc1a5986b780d9bb0fcf43b6bdab2a
shooooting <shooootingsta@gmail.com>
2020-06-20 22:09:22 +0900

    1. Add HeaderView  - autolayout  - ImagePicker

--- WorkspaceSettings.xcsettings  FabricCareSymbols/WorkspaceSettings.xcsettings  FabricCareSymbols/View/ClotheInfoCustomCell.swift
+++ WorkspaceSettings.xcsettings  FabricCareSymbols/WorkspaceSettings.xcsettings  FabricCareSymbols/View/ClotheInfoCustomCell.swift
@@ -9,50 +9,70 @@
 import UIKit
 
 class ClotheInfoCustomCell: UITableViewCell {
-  static var identifier = "ClotheInfoCustomCell"
-  let imgView: UIImageView = {
-    let img = UIImageView()
-    img.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
-    img.layer.cornerRadius = img.frame.width / 2
-    img.layer.masksToBounds = true
-    return img
-  }()
-  
-  let label: UILabel = {
-    let label = UILabel()
-    label.textAlignment = .center
-    label.layer.borderWidth = 2
-    label.layer.borderColor = CGColor(srgbRed: 17/255, green: 154/255, blue: 237/255, alpha: 1)
-    label.layer.cornerRadius = 7
-    return label
-  }()
-  
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    static var identifier = "ClotheInfoCustomCell"
+
+    let imgButton: UIButton = {
+        let imgButton = UIButton()
+        imgButton.backgroundColor = .systemGray2
+        imgButton.setImage(UIImage(named: "plus_photo"), for: .normal)
+        return imgButton
+    }()
     
-    contentView.addSubview(imgView)
-    contentView.addSubview(label)
+    let textField: UITextField = {
+        let text = UITextField()
+        text.placeholder = " 이름을 적어주세요."
+        text.textAlignment = .center
+        text.keyboardType = .emailAddress
+        text.layer.borderWidth = 2
+        text.layer.borderColor = CGColor(srgbRed: 17/255, green: 154/255, blue: 237/255, alpha: 1)
+        text.layer.cornerRadius = 7
+        text.clearButtonMode = .whileEditing
+        return text
+    }()
     
-    imgView.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-      imgView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
-      imgView.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: 25),
-      imgView.heightAnchor.constraint(equalToConstant: 150),
-      imgView.widthAnchor.constraint(equalToConstant: 150)
-    ])
+    let view: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGreen
+        return view
+    }()
     
-    label.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-      label.leadingAnchor.constraint(equalTo: imgView.trailingAnchor, constant: 10),
-      label.trailingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 360),
-      label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 75),
-      label.heightAnchor.constraint(equalToConstant: 50),
-      label.widthAnchor.constraint(equalToConstant: 200)
-    ])
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let width: CGFloat = contentView.frame.width / 2 - 10
+        let ui = [imgButton, textField, view]
+        ui.forEach { contentView.addSubview($0) }
+        ui.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        imgButton.layer.cornerRadius = width / 2
+
+        NSLayoutConstraint.activate([
+            imgButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            imgButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            imgButton.widthAnchor.constraint(equalToConstant: width),
+            imgButton.heightAnchor.constraint(equalToConstant: width)
+        ])
+        
+        NSLayoutConstraint.activate([
+            textField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            textField.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 50),
+            textField.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2),
+            textField.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            view.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20),
+            view.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2),
+            view.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5)
+        ])
+        
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
+
